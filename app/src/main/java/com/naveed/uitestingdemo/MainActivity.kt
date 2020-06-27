2f74b27e5df1cc4a9b41936af6bdbd2614fee4ad
Naveed <nak411@gmail.com>
2020-06-26 23:52:14 -0400

    Initial commit

---
+++ app/src/main/java/com/naveed/uitestingdemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.naveed.uitestingdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

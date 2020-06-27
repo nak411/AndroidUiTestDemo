1163806998cae1865758da6b441573e75a1a681c
Naveed <nak411@gmail.com>
2020-06-27 18:51:21 -0400

    Create second activity

--- app/src/main/java/com/naveed/uitestingdemo/MainActivity.kt
+++ app/src/main/java/com/naveed/uitestingdemo/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.naveed.uitestingdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_hello.setOnClickListener {
+            simulateApiCallAndLaunch()
+        }
+    }
+
+    private fun simulateApiCallAndLaunch() {
+        val intent = Intent(this, SecondaryActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/naveed/uitestingdemo/SecondaryActivity.kt
@@ -0,0 +1,12 @@
+package com.naveed.uitestingdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secondary)
+    }
+}
